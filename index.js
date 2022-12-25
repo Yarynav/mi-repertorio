11fdab2a0215ccd38f426d21398244ebae7ffe4b
yary navarrete <yar.navarrete@gmail.com>
2022-12-25 18:52:38 -0300

    api-2

--- index.js
+++ index.js
@@ -17,7 +17,13 @@ app.get('/canciones', async (req, res) => {
   const songs = await fs.readFile('./repertorio.json', 'utf8');
   res.json(JSON.parse(songs));
 });
-app.post('/canciones', async (req, res) => {});
+app.post('/canciones', async (req, res) => {
+  const songs = await fs.readFile('./repertorio.json', 'utf8');
+  const songsJson = JSON.parse(songs);
+  songsJson.push(req.body);
+  await fs.writeFile('./repertorio.json', JSON.stringify(songsJson, null, 2));
+  res.json(JSON.parse(songs));
+});
 app.put('/canciones/:id', (req, res) => {
   res.json({});
 });
